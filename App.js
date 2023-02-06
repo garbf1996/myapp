245b9d6e470151ddc754063fbbfe1cd962455b15
garber <garbfbatista@gmail.com>
2023-02-05 21:11:00 -0400

    tarea 3

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Gyroscope } from "expo-sensors";
 
 export default function App() {
+  const [{ x, y, z }, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
+
+  const _slow = () => Gyroscope.setUpdateInterval(1000);
+  const _fast = () => Gyroscope.setUpdateInterval(16);
+  //Funcion para suscribirse al sensor
+  const _subscribe = () => {
+    setSubscription(
+      Gyroscope.addListener((gyroscopeData) => {
+        setData(gyroscopeData);
+      })
+    );
+  };
+  //Funcion para desuscribirse al sensor
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Giroscopio:</Text>
+      <Text style={styles.text}>x: {x}</Text>
+      <Text style={styles.text}>y: {y}</Text>
+      <Text style={styles.text}>z: {z}</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={subscription ? _unsubscribe : _subscribe}
+          style={styles.button}
+        >
+          <Text>{subscription ? "1" : "0"}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={_slow}
+          style={[styles.button, styles.middleButton]}
+        >
+          <Text>Lento</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_fast} style={styles.button}>
+          <Text>Rapido</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,8 +61,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingHorizontal: 10,
+  },
+  text: {
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#eee",
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: "#ccc",
   },
 });
